f0ef8208985f891660b327e7f2b2752883046a63
猫吉 <freeaddress834@gmail.com>
2021-02-20 23:00:31 +0900

    add: 税込金額の算出を記述

--- AkiosTask12-2/ViewController.swift
+++ AkiosTask12-2/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 final class ViewController: UIViewController {
 
-    @IBOutlet weak var tasExluededPriceTextField: UITextField!
+    @IBOutlet weak var taxExluededPriceTextField: UITextField!
     @IBOutlet weak var taxPercentageTextField: UITextField!
     @IBOutlet weak var taxIncludedPriceLabel: UILabel!
 
@@ -19,8 +19,13 @@ final class ViewController: UIViewController {
     }
 
     @IBAction func calculateTaxIncludedPriceButton(_ sender: UIButton) {
+        guard let taxExcludedPrice = Int(taxExluededPriceTextField.text!),
+              let taxPercentage = Int(taxPercentageTextField.text!) else {
+            return
+        }
+        let taxPrice = Int(Double(taxExcludedPrice) * (Double(taxPercentage) / 100))
+        taxIncludedPriceLabel.text = "\(taxPrice + taxExcludedPrice)"
     }
-
 
 }
 
